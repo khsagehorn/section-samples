02d91dd022c03d7b2ffc82c0452a5f134834bc82
Klaus Hunter Sagehorn <huntersagehorn@gmail.com>
2021-03-29 02:56:34 -0400

    Update strainge.js

--- strainge-ness/strainge.js
+++ strainge-ness/strainge.js
@@ -1,25 +1,24 @@
 // Establish html data attibute that can be accessed via CSS for homepage nav behaviors
 $(function(){
-	var hero = $('.mx-intro-sec');
-	if (hero){
-    var menu = $('.main-header');
-	  var subNav = $('.dropdown-menu');
-		// set a 'scroll' data attribute on the html tag
-		document.documentElement.dataset.scroll = 0;
-		document.addEventListener('scroll', function(event){
-			document.documentElement.dataset.scroll = window.scrollY;
-		});
+    var hero = $('.mx-intro-sec');
+    if (hero){
+        var menu = $('.main-header');
+        var subNav = $('.dropdown-menu');
+	    // set a 'scroll' data attribute on the html tag
+	document.documentElement.dataset.scroll = 0;
+	document.addEventListener('scroll', function(event){
+	    document.documentElement.dataset.scroll = window.scrollY;
+	});
 
-	  document.body.addEventListener("mousemove", function(event) {
-    //Check if we are in the top area of the page, or if we've moved but the sub-menu is still active.
-      if(event.pageY < 90 || subNav.hasClass('show')) {
-			  menu.addClass('show');
+	document.body.addEventListener("mousemove", function(event) {
+        //Check if we are in the top area of the page, or if we've moved but the sub-menu is still active.
+        if(event.pageY < 90 || subNav.hasClass('show')) {
+	    menu.addClass('show');
+        } else {
+	    menu.removeClass('show');
       }
-		  else {
-			  menu.removeClass('show');
-		  }
-	  });
-	}
+   });
+  }
 });
 
 // Play footer vine animation when in view
@@ -44,9 +43,9 @@ $(function(){
 // accordion handler
 if ($('.mx-accordion-content').length){
     $('.accordion-title').on('click', function(){
-		if(!($(this).parent().hasClass('active'))){
-		    $('.accordion').removeClass('active');
-		    $(this).parent().addClass('active');
-		} else { $('.accordion').removeClass('active'); }
+        if(!($(this).parent().hasClass('active'))){
+	    $('.accordion').removeClass('active');
+	    $(this).parent().addClass('active');
+	} else { $('.accordion').removeClass('active'); }
     });
 }
